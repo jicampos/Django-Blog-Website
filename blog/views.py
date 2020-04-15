7daaecc6afac03092444decead5ade61b744d904
javier <javic9788@gmail.com>
2020-04-15 14:34:59 -0500

    init commit

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+posts = [
+    {
+        "author": "Javier Campos",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "August 27, 2020"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "August 28, 2020"
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, "blog/home.html", context)
+
+def about(request):
+    return render(request, "blog/about.html")
