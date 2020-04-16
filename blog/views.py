3c32e965ba69c2dc4a19fad72005b5457b7ccbd5
javier <javic9788@gmail.com>
2020-04-16 15:49:39 -0500

    add database and admin settings

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        "author": "Javier Campos",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "August 27, 2020"
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "August 28, 2020"
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, "blog/home.html", context)
